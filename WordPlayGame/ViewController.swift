9c1e2731d52e515609e187feff0bf5a2a657dfd3
student1 <student1@jh-mbair-158-08.d214.org>
2015-12-08 10:15:36 -0600

    Initial Commit

---
+++ WordPlayGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordPlayGame
+//
+//  Created by student1 on 12/8/15.
+//  Copyright © 2015 John Hersey High school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
