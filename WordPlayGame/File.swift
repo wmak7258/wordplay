dfc483903093e8c0ff8957cfab9d63000d7e1a71
student1 <student1@jh-mbair-158-08.d214.org>
2015-12-08 15:48:25 -0600

    add IBOutlet and file.swift

---
+++ WordPlayGame/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  WordPlayGame
+//
+//  Created by student1 on 12/8/15.
+//  Copyright © 2015 John Hersey High school. All rights reserved.
+//
+
+import Foundation
+
+class finalSentence {
+    var noun = String()
+    var verb = String()
+    var adjective = String()
+    
+    
+    
+}
